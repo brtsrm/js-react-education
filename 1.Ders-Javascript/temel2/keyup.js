1d43070a7a42473e2f66013b6b80cc0db8a87627
brtsrm <bsarmis@outlook.com>
2021-04-12 16:06:41 +0300

    keyup kullanımı

---
+++ 1.Ders-Javascript/temel2/keyup.js
@@ -0,0 +1,13 @@
+const form = document.querySelector(".signup")
+
+const kullaniciDesen = /^[a-z]{5,7}$/;
+form.kullaniciAd.addEventListener('keyup', (e) => {
+    
+    if (kullaniciDesen.test(e.target.value)) {
+        form.kullaniciAd.setAttribute('class', 'basarili')
+    }
+    else {
+        form.kullaniciAd.setAttribute('class', 'hatali')
+
+    }
+})
