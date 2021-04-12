c5b9a06f1a40e2a9a7987f602b6b97a3a3acd7fc
brtsrm <bsarmis@outlook.com>
2021-04-12 18:02:37 +0300

    Reverse kullanımı

---
+++ 1.Ders-Javascript/temel3/reverse/script.js
@@ -0,0 +1,29 @@
+const metinDizi = ['metin', 'ali', 'berat'];
+
+
+console.log(metinDizi.sort())
+
+console.log(metinDizi.reverse())
+
+
+const sayi = [12, 13, 14, 15, 16, 17]
+
+console.log(sayi.sort())
+
+console.log(sayi.reverse())
+
+
+const nesneDizisi = [
+    { isim: 'Berat', not: 60 },
+    { isim: 'Medine', not: 62 },
+    { isim: 'Kerem', not: 61 },
+    { isim: 'Zeynep', not: 63 },
+]
+
+nesneDizisi.sort((ikinci,birinci) => {
+     if(ikinci.isim>birinci.isim)
+     {
+         return -1;
+     }
+})
+console.log(nesneDizisi)
