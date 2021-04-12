8ab945d8b90b48d922f9e8ca0e6e43995ce3e711
brtsrm <bsarmis@outlook.com>
2021-04-12 17:52:17 +0300

    Sort Kullanımı

---
+++ 1.Ders-Javascript/temel3/sort/script.js
@@ -0,0 +1,32 @@
+const metinDizi = ['metin', 'ali', 'berat'];
+
+
+metinDizi.sort();
+// console.log(metinDizi)
+
+metinDizi.reverse();
+// console.log(metinDizi)
+
+
+const sayiDizisi = [12, 45, 78, 88, 97, 85];
+sayiDizisi.sort()
+// console.log(sayiDizisi)
+sayiDizisi.reverse()
+// console.log(sayiDizisi)
+
+
+const nesneDizi = [
+    { isim: 'zeynep', not: 26 },
+    { isim: 'berat', not: 56 },
+    { isim: 'kerem', not: 76 },
+    { isim: 'medine', not: 86 },
+]
+nesneDizi.sort((ilk, ikinci) => {
+    if (ilk.not < ikinci.not) {
+        return -1;
+    } else if (ilk.not > ikinci.not) {
+        return 1;
+    } else {
+        return 0;
+    }
+})
