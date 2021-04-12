72efb16370e30d736d01b481ff50a6cf29cd4376
brtsrm <bsarmis@outlook.com>
2021-04-12 12:16:08 +0300

    addEventListener kullanimi

---
+++ 1.Ders-Javascript/temel2/event.js
@@ -0,0 +1,28 @@
+const button = document.querySelector("button")
+
+// button.addEventListener('click',() => {
+//     console.log("merhaba berat")
+// })
+
+const input = document.querySelector("input")
+
+input.addEventListener("click", (e) => {
+    console.log(button.disabled);
+    if (!input.checked) {
+        button.disabled = true
+        button.innerText = "Pasif"
+
+    } else {
+        button.disabled = false
+        button.innerText = "Aktif"
+    }
+})
+
+const li = document.querySelectorAll("li")
+
+li.forEach(i => {
+    i.addEventListener('click', (e) => {
+        // console.log(e.target);
+        e.target.style = 'text-decoration:line-through'
+    })
+})
