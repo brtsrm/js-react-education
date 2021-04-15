c5579c8f45054df938a259c44a9a72921d7db757
brtsrm <bsarmis@outlook.com>
2021-04-15 10:59:18 +0300

    Promise Kullanımı

---
+++ 1.Ders-Javascript/orta1/promise/script.js
@@ -0,0 +1,19 @@
+// DIş kaynaktan veri alınırken bir süre beklemesi
+
+    
+    return new Promise((resolve,reject) => {
+        // Resolve doğru veriyi gösteriri
+        resolve('Veriler Çekildi')
+        // Reject Hatalı işlemi gösterir
+        reject("Hatalı istek")
+    })
+}
+
+verileriGetir().then(veri => {
+    // doğru veriyi çalıştırı
+    console.log(veri)
+}).catch((err) => {
+    console.log(err)
+
+    // Hatalı veriyi çalışıtır
+})
