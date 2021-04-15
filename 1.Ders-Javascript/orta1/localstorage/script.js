3f34db57dbb2ff2844a81abcdcf1060b3af3ee94
brtsrm <bsarmis@outlook.com>
2021-04-15 17:11:31 +0300

    local storage kullanımı

--- 1.Ders-Javascript/orta1/localstorage/script.js
+++ 1.Ders-Javascript/orta1/localstorage/script.js
@@ -1,1 +1,16 @@
+// local storage veri ekleme
 localStorage.setItem("isim","ali");
+localStorage.setItem("yas",21)
+
+
+let isim = localStorage.getItem("isim")
+
+console.log(isim)
+// localstorage'teki veriyi çağırma
+let yas = localStorage.getItem("yas");
+console.log(yas)
+// local storage silme
+localStorage.removeItem("yas")
+
+// Hepsini siler
+localStorage.clear()
