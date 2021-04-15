6f2ba1c130b3093178894f4c8b3dbe4871c9bfa2
brtsrm <bsarmis@outlook.com>
2021-04-15 11:04:08 +0300

    Promise ile dış kaynaktan veri çekme

--- 1.Ders-Javascript/orta1/promise/script.js
+++ 1.Ders-Javascript/orta1/promise/script.js
@@ -3,14 +3,23 @@
 const verileriGetir = (kaynak) => {
     
     return new Promise((resolve,reject) => {
-        // Resolve doğru veriyi gösteriri
-        resolve('Veriler Çekildi')
-        // Reject Hatalı işlemi gösterir
-        reject("Hatalı istek")
+
+        const request = new XMLHttpRequest();
+        request.addEventListener("readystatechange",() => {
+            if(request.readyState == 4 && request.status == 200){
+                let veriler = JSON.parse(request.responseText)
+                resolve(veriler)
+            }else if(request.readyState == 4){
+                reject("Bir Hata oluştu")
+            }
+        });
+        request.open("GET",kaynak)
+        request.send()
     })
 }
 
-verileriGetir().then(veri => {
+// verileriGetir("veriler.json").then(veri => {
+verileriGetir("https://jsonplaceholder.typicode.com/todos").then(veri => {
     // doğru veriyi çalıştırı
     console.log(veri)
 }).catch((err) => {
