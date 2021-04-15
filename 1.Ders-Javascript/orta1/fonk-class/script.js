f13b41a666881757c670d0bc999271209557dc53
brtsrm <bsarmis@outlook.com>
2021-04-15 14:07:40 +0300

    Fonksiyon sınıflarında miras alma

--- 1.Ders-Javascript/orta1/fonk-class/script.js
+++ 1.Ders-Javascript/orta1/fonk-class/script.js
@@ -10,10 +10,28 @@ function Personel(ad, birim, gorev) {
 
 Personel.prototype.isYap = function () {
     console.log(`${this.ad} iş yapıldı`)
-    
+
+}
+
+function Yonetici(ad, birim, gorev, unvan) {
+
+    Personel.call(this, ad, birim, gorev)
+    this.unvan = unvan;
+
+
+}
+Yonetici.prototype = Object.create(Personel.prototype)
+Yonetici.prototype.personelKov = function(){
+    console.log("Personel kovuldu");
 }
 
 const personel = new Personel("Berat", "Bilgi işlem", "Yazılım uzmanı")
+const yonetici = new Yonetici('Cem', "Team Leader", "Ceo", "Dijital God")
 
 console.log(personel)
+console.log(yonetici)
+
+
 personel.isYap()
+yonetici.isYap()
+yonetici.personelKov()
