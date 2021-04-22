dd9ee803ee87acd56e3a3a8f9d35fc6f6c009786
brtsrm <bsarmis@outlook.com>
2021-04-22 17:41:28 +0300

    Package ve npm kullanımı

---
+++ 2.Ders-Nodejs/8. Package and NPM/server.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs")
+
+
+const _ = require("lodash")
+
+const rastgele = _.random(0,50)
+
+console.log(rastgele)
+
+const server = http.createServer((req, res) => {
+
+
+    let path = './html/';
+
+
+    switch (req.url) {
+        case "/":
+            path += "/index.html";
+            res.statusCode = 200;
+            break;
+        case "/iletisim":
+            path += "iletisim.html";
+            res.statusCode = 200;
+            break;
+        case "/hakkimizda":
+            path += "hakkimizda.html";
+            res.statusCode = 200;
+            break;
+        default:
+            path += "404.html"
+            res.statusCode = 404
+    }
+
+    res.setHeader("Content-Type", "text/html")
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.write(data)
+            res.end()
+        }
+    })
+
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log("3000. port dinleniysor.")
+})
