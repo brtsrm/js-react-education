89e64b346cda90a4a193265a0f4dda5e86b8f1ee
brtsrm <bsarmis@outlook.com>
2021-04-22 15:32:24 +0300

    Node js

---
+++ 2.Ders-Nodejs/2. Global nesnesi/basic.js
@@ -0,0 +1,13 @@
+// global.setTimeout(() => {
+//     console.log("3 saniye geçti ve başlatıyoruz bu fonksiyonu")
+// }, 3000);
+
+
+// setInterval(() => {
+//     console.log("merhaba")
+// }, 500);
+
+
+console.log(__dirname)
+
+console.log(__filename)
