de1a187390527fc97237e70949d335534b2aac68
brtsrm <bsarmis@outlook.com>
2021-05-09 01:02:02 +0300

    useEffect kullanımı

--- temel-react-hook-app/src/App.js
+++ temel-react-hook-app/src/App.js
@@ -1,10 +1,12 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import "./App.css"
 
 function App ()  {
 
   const [bardakSayisi,barkSayisiniDegistir] = useState(0);
   const [metin,setMetin] = useState({text:'İçtiğim Su bardak adeti'});
+  const [bardakStyle,setbardakStyle] = useState("bardak"+0);
+
   const suIctim = () => {
     barkSayisiniDegistir(bardakSayisi+1)
     setMetin({
@@ -12,9 +14,13 @@ function App ()  {
       text:'İçtiğim su sayısı' + bardakSayisi
     })
   }
+  useEffect(() => {
+    setbardakStyle('bardak' + bardakSayisi)
+  },[bardakSayisi])
+
   return(
-   <div>
-     <h3>{bardakSayisi}</h3>
+   <div className="app">
+     <h3 className={bardakStyle}>{bardakSayisi}</h3>
      <p>{metin.text}</p>
      <button onClick={suIctim}>Su içtim</button>
    </div>
